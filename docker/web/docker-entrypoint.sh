f13f6573fada80291557fcca4eb3615093469c17
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2022-04-18 12:05:11 +0800

    the apache-sites-enabled added and customized

---
+++ docker/web/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+#composer install --no-suggest --no-interaction --no-scripts --no-progress --prefer-dist
+
+if [ ! -e /var/www/randstad-onboarding/db.lock ]; then
+  #php artisan migrate:fresh
+  #php artisan passport:install
+  #php artisan db:seed --class=LiveDatabaseTableSeeder
+  #php artisan copy:on-boarding-data
+  #php artisan db:seed --class=TrainingStreamColumnFullStreamToActiveStreamTableColumnSeeder
+  #touch /var/www/randstad-onboarding/db.lock
+fi
+
+docker-php-entrypoint apache2-foreground
